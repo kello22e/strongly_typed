62e5fb8c742ed2e732caba550ff7741cc49d2e86
Emily Kellogg <kello22e@mtholyoke.edu>
2018-07-18 15:45:12 -0500

    lol

--- README.md
+++
@@ -1,1 +0,0 @@
-# strongly_typed
--- app.rb
+++ app.rb
@@ -1,11 +1,11 @@
 require 'sinatra'
-require 'bundler'
-Bundler.require
+#require 'bundler'
+#Bundler.require
 class App < Sinatra::Base
 
   get '/' do
     erb :index
-    #File.read(File.join('public', 'index.html'))
+
   end
 
 end
